d765d5ce9edc64c3d2ad5c0bd7beba7bb00da979
Loïc Daudé Mondet <loic.daude@mondet.fr>
2021-03-23 21:23:19 +0100

    Removing all ffmpeg stuff + get pixels arrays
    Removing all  ffmpeg related stuff and add code to get top, left, right and bottom pixels arrays

--- DeltaV_OpenCV_Python/main.py
+++ DeltaV_OpenCV_Python/main.py
@@ -3,25 +3,23 @@
 import numpy as np
 import cv2 as cv
 from sys import argv
-
+import time
 
 # Reads /dev/video device index from first command line argument
 dev_index = int(argv[1])
-# Framerate is 1000ms / (second command line argument)
-framerate = 1000 // int(argv[2])
+# Framerate is second command line argument
+framerate = int(argv[2])
 # Width from 3rd argument
 width = int(argv[3])
 # Height for 4th argument
 height = int(argv[4])
 
-# First try to open video capture from v4l2 device /dev/video{device}
+# First try to open video capture from camera device /dev/video{device}
 print("Creating video capture...")
 video = cv.VideoCapture(dev_index)
 print("Created video capture:", video)
 
-# As ffmpeg is background process, it might not be running at this point, so
-# v4l2 device may not be open.
-while not video.isOpened():  # While v4l2 device haven't been opened by ffmpeg
+while not video.isOpened():  # While camera is device is not opened
     print("Opening capture...")
     video.open(dev_index)  # Tries again to get video capture from device
 
@@ -29,10 +27,24 @@ print("Opened.")
 
 while True:
     received, frame = video.read()  # Reads and waits for next captured frame
-    squared = cv.resize(frame, (width, height))
-
     if not received:  # No longer signal received from device, stops
         print("No signal from video stream.")
         break
-
-    print(squared)
+    frame_recized = cv.resize(frame, (width, height))
+    if height > 1: # we make sure that we can find left and right pixels
+        array_top_pixels = frame_recized[0] # top pixels are the first row of the frame
+        array_left_pixels = np.zeros((height-2, 3), dtype=np.uint8) # initialization of left pixels array with zeros and the right shape
+        array_right_pixels = np.zeros((height-2, 3), dtype=np.uint8) # same for right pixels array
+        for i in range(1, height-1, 1):
+            array_left_pixels[i-1] = frame_recized[i][0] # left pixels are the first pixels of each row in the frame (except the first and last row)
+            array_right_pixels[i-1] = frame_recized[i][width-1] # same but they are the last
+        array_bottom_pixels = frame_recized[height-1] # bottom pixels are the last row of the frame
+    else: #otherwise  everything in the same as the top pixels (the first row in the frame)
+        array_top_pixels = frame_recized[0]
+        array_left_pixels, array_right_pixels, array_bottom_pixels =  array_top_pixels, array_top_pixels, array_top_pixels
+    # printing some info in the console
+    print("pixels du haut :" , array_top_pixels)
+    print("pixels de gauche :" , array_left_pixels)
+    print("pixels de droite :" , array_right_pixels)
+    print("pixels du bas :" , array_bottom_pixels)
+    time.sleep(1/framerate)
