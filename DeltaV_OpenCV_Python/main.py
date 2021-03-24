fe2163c9f1b5821bdf4773e45bd72956a7ad2225
ThisALV <lel.charriere@orange.fr>
2021-03-24 09:32:37 +0100

    Clean code formatting

--- DeltaV_OpenCV_Python/main.py
+++ DeltaV_OpenCV_Python/main.py
@@ -30,21 +30,24 @@ while True:
     if not received:  # No longer signal received from device, stops
         print("No signal from video stream.")
         break
-    frame_recized = cv.resize(frame, (width, height))
-    if height > 1: # we make sure that we can find left and right pixels
-        array_top_pixels = frame_recized[0] # top pixels are the first row of the frame
-        array_left_pixels = np.zeros((height-2, 3), dtype=np.uint8) # initialization of left pixels array with zeros and the right shape
-        array_right_pixels = np.zeros((height-2, 3), dtype=np.uint8) # same for right pixels array
+
+    frame_resized = cv.resize(frame, (width, height))
+    if height > 1:  # we make sure that we can find left and right pixels
+        array_top_pixels = frame_resized[0]  # top pixels are the first row of the frame
+        array_left_pixels = np.zeros((height-2, 3), dtype=np.uint8)  # initialization of left pixels array with zeros and the right shape
+        array_right_pixels = np.zeros((height-2, 3), dtype=np.uint8)  # same for right pixels array
         for i in range(1, height-1, 1):
-            array_left_pixels[i-1] = frame_recized[i][0] # left pixels are the first pixels of each row in the frame (except the first and last row)
-            array_right_pixels[i-1] = frame_recized[i][width-1] # same but they are the last
-        array_bottom_pixels = frame_recized[height-1] # bottom pixels are the last row of the frame
-    else: #otherwise  everything in the same as the top pixels (the first row in the frame)
-        array_top_pixels = frame_recized[0]
-        array_left_pixels, array_right_pixels, array_bottom_pixels =  array_top_pixels, array_top_pixels, array_top_pixels
+            array_left_pixels[i-1] = frame_resized[i][0]  # left pixels are the first pixels of each row in the frame (except the first and last row)
+            array_right_pixels[i-1] = frame_resized[i][width-1]  # same but they are the last
+        array_bottom_pixels = frame_resized[height-1]  # bottom pixels are the last row of the frame
+    else:  # otherwise  everything in the same as the top pixels (the first row in the frame)
+        array_top_pixels = frame_resized[0]
+        array_left_pixels, array_right_pixels, array_bottom_pixels = array_top_pixels, array_top_pixels, array_top_pixels
+
     # printing some info in the console
-    print("pixels du haut :" , array_top_pixels)
-    print("pixels de gauche :" , array_left_pixels)
-    print("pixels de droite :" , array_right_pixels)
-    print("pixels du bas :" , array_bottom_pixels)
+    print("pixels du haut :", array_top_pixels)
+    print("pixels de gauche :", array_left_pixels)
+    print("pixels de droite :", array_right_pixels)
+    print("pixels du bas :", array_bottom_pixels)
+
     time.sleep(1/framerate)
