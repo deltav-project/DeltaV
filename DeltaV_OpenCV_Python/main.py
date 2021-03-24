d7bbeccca8ab0bcc0f86bb9aed699a6de16d4dd1
ThisALV <lel.charriere@orange.fr>
2021-03-24 17:27:12 +0100

    Improve framerate delaying

--- DeltaV_OpenCV_Python/main.py
+++ DeltaV_OpenCV_Python/main.py
@@ -14,6 +14,10 @@ width = int(argv[3])
 # Height for 4th argument
 height = int(argv[4])
 
+unlimited_framerate = framerate == 0  # 0fps enable unlimited fps mode
+if not unlimited_framerate:  # Avoid ArithmeticError with div by 0
+    framerate_delay_s = 1 / framerate  # Delay expected between each frames (main loop iteration)
+
 # First try to open video capture from camera device /dev/video{device}
 print("Creating video capture...")
 video = cv.VideoCapture(dev_index)
@@ -26,6 +30,8 @@ while not video.isOpened():  # While camera is device is not opened
 print("Opened.")
 
 while True:
+    image_handling_begin = time.time()  # Save image handling begin timestamp
+
     received, frame = video.read()  # Reads and waits for next captured frame
     if not received:  # No longer signal received from device, stops
         print("No signal from video stream.")
@@ -44,4 +50,7 @@ while True:
         array_top_pixels = frame_resized[0]
         array_left_pixels, array_right_pixels, array_bottom_pixels = array_top_pixels, array_top_pixels, array_top_pixels
 
-    time.sleep(1/framerate)
+    image_handling_duration = time.time() - image_handling_begin  # Save image handling duration
+
+    if not unlimited_framerate:  # Wait if and only if framerate should be limited
+        time.sleep(framerate_delay_s - image_handling_duration)  # Wait time remaining before next frame should be handled
